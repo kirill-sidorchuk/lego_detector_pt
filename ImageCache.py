868a5e6b7f62a2eb4be76b7989167cb042b93162
kirill.sidorchuk <kirill.sidorchuk@teamdev.com>
2020-07-10 17:09:51 +0300

    Make square crops for test set

--- ImageCache.py
+++ ImageCache.py
@@ -1,6 +1,5 @@
 import cv2
 import numpy as np
-from typing import Tuple
 
 
 class ImageCache:
@@ -8,7 +7,7 @@ class ImageCache:
     Caches images into RAM. Image is loaded from disk only once. After loading image is kept in RAM in unpacked format.
     """
 
-    def __init__(self, force_rgb: bool, dst_img_size: Tuple[int, int] or None = None):
+    def __init__(self, force_rgb: bool, dst_img_size: int or None = None):
         self.cache = {}
         self.forceRgb = force_rgb
         self.dst_img_size = dst_img_size
@@ -26,6 +25,15 @@ class ImageCache:
             if img is None:
                 raise Exception('Failed to load image: ' + filename)
             if self.dst_img_size is not None:
+                # making image square
+                square_side = min(img.shape[0], img.shape[1])
+                offset = (max(img.shape[0], img.shape[1]) - square_side) // 2
+                if offset != 0:
+                    if img.shape[0] > img.shape[1]:
+                        img = img[offset: offset + square_side, :, :]
+                    else:
+                        img = img[:, offset: offset + square_side, :]
+
                 img = cv2.resize(img, self.dst_img_size, interpolation=cv2.INTER_AREA)
             self.cache[filename] = img
 
